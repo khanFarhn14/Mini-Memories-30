715c4961039667087661a7b1617f77f2bd5539c6
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-29 16:14:11 +0530

    feat(route): added initial route

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_memories_30/firebase_options.dart';
-import 'package:mini_memories_30/pages/page_home.dart';
+import 'package:mini_memories_30/user_interface/pages/routes/route_name.dart';
+import 'package:mini_memories_30/user_interface/pages/routes/routes.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PageHome(),
+      initialRoute: RouteName.username,
+      onGenerateRoute: Routes.generateRoute,
     );
   }
 }
