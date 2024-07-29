559a1813e9dcd0d5ac61a5e0376b38b58ebb8ccb
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-29 16:16:54 +0530

    feat: intialization of the page username

---
+++ lib/user_interface/pages/page_username.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PageUsername extends StatefulWidget {
+  const PageUsername({super.key});
+
+  @override
+  State<PageUsername> createState() => _PageUsernameState();
+}
+
+class _PageUsernameState extends State<PageUsername> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
