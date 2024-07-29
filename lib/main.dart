cbd9747a0211a201c28d90ee6c2813ad3e2fd6eb
Khan Farhan <thecubeshade21@gmail.com>
2024-07-29 16:02:06 +0530

    feat(firebase): added init for firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_memories_30/firebase_options.dart';
 import 'package:mini_memories_30/pages/page_home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
