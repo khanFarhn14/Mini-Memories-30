19e5664a8aea444401cb301414f78118a3150440
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-29 16:15:39 +0530

    feat(route name): initialize route name class

---
+++ lib/user_interface/pages/routes/route_name.dart
@@ -0,0 +1,8 @@
+class RouteName {
+
+  // ---------- Sign In ----------
+  static const String username = 'page_username';
+
+  // ---------- Authorized Pages ----------
+  static const String home = 'page_home';
+}
