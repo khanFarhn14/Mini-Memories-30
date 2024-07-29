c30ba2caba7333bb3184e48cf433b9b478347183
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-29 17:23:30 +0530

    refractor: new directory routes

---
+++ lib/user_interface/routes/route_name.dart
@@ -0,0 +1,6 @@
+class RouteName {
+
+  static const String username = 'page_username';
+  static const String home = 'page_home';
+  static const String recordVideo = 'page_record_video'; 
+}
---
+++ lib/user_interface/routes/routes.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:mini_memories_30/user_interface/pages/page_home.dart';
+import 'package:mini_memories_30/user_interface/pages/page_record_video.dart';
+import 'package:mini_memories_30/user_interface/pages/page_username.dart';
+import 'package:mini_memories_30/user_interface/routes/route_name.dart';
+
+class Routes{
+  static Route<dynamic> generateRoute(RouteSettings settings){
+    switch(settings.name){
+      // ---------- Page Username ----------
+      case RouteName.username:
+        return MaterialPageRoute(builder: (context) => const PageUsername());
+        
+      case RouteName.home:
+        return MaterialPageRoute(builder: (context) => const PageHome());
+
+      case RouteName.recordVideo:
+        return PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) => const PageRecordVideo(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child){
+            return SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(0,1),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            );
+          }
+        );
+
+      default: return MaterialPageRoute(
+        builder: (context){
+          return const Scaffold(
+            body: Center(
+              child: Text('No Route Defined'),
+            ),
+          );
+        }
+      );
+    }
+  }
+}
