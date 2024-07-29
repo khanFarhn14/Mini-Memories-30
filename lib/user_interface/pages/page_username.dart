7b3250cf1dfa738335d6aad27f1125a7385ca90c
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-29 16:37:52 +0530

    feat: created user input field for username

--- lib/user_interface/pages/page_username.dart
+++ lib/user_interface/pages/page_username.dart
@@ -8,14 +8,50 @@ class PageUsername extends StatefulWidget {
 }
 
 class _PageUsernameState extends State<PageUsername> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _usernameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 52, horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Mini-Memories-30", style: Theme.of(context).textTheme.headlineMedium,),
+              const SizedBox(height: 24),
+
+              Text(
+                "Enter a username",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              
+              const SizedBox(height: 12,),
+
+              Form(
+                key: _formKey,
+                child: TextFormField(
+                  controller: _usernameController,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                )
+              ),
+              const SizedBox(height: 48,),
+
+              ElevatedButton(
+                onPressed: (){},
+                child: Text("Submit", style: Theme.of(context).textTheme.labelMedium,)
+              )
+            ],
+          ),
         ),
       ),
     );
