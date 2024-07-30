2c718422059861955a8b952a05696a53185bbe2e
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-30 12:00:39 +0530

    feat(record video): added recordVideo function

--- lib/user_interface/pages/page_home.dart
+++ lib/user_interface/pages/page_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mini_memories_30/user_interface/routes/route_name.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mini_memories_30/user_interface/pages/page_new_record_video.dart';
 
 class PageHome extends StatefulWidget {
   const PageHome({super.key});
@@ -9,6 +10,36 @@ class PageHome extends StatefulWidget {
 }
 
 class PageHomeState extends State<PageHome> {
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  _recordVideo() async{
+    try{
+      final ImagePicker picker = ImagePicker();
+      final XFile? cameraVideo = await picker.pickVideo(
+        source: ImageSource.camera,
+        maxDuration: const Duration(seconds: 15),
+        preferredCameraDevice: CameraDevice.front,
+      );
+      if(cameraVideo == null){
+        clearPrint("No Capture Null");
+        return;
+      }
+
+      clearPrint("Video Capture: ${cameraVideo.path}");
+      final route = MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (_) => PageNewRecordVideo(videoPath: cameraVideo.path),
+      );
+      // ignore: use_build_context_synchronously
+      Navigator.push(context, route);
+    }catch(exception){
+      clearPrint("Exception caught in _recordVideo: $exception");
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +49,7 @@ class PageHomeState extends State<PageHome> {
 
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          Navigator.pushNamed(context, RouteName.recordVideo);
+          _recordVideo();
         },
         child: const Icon(
           Icons.video_camera_back_outlined
