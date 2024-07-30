fbeb473f422937456f73f1aeb7e1814a25a3eb38
khanFarhn14 <thecubeshade21@gmail.com>
2024-07-30 16:10:02 +0530

    feat(save metadata): adding function to save metadata of the video

--- lib/backend/upload_file.dart
+++ lib/backend/upload_file.dart
@@ -1,38 +1,54 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:mini_memories_30/user_interface/pages/page_upload_video.dart';
 
 class UploadFile {
+  BuildContext context;
+  UploadFile({required this.context});
+
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-  Future<void> uploadVideo({required String videoTitle, required String videoPath}) async {
+  Future<void> uploadVideo({required String username,required String videoTitle, required String videoPath, required int duration}) async {
     final File file = File(videoPath);
     
     // Check if file exists
     if (!await file.exists()) {
-      clearPrint("File does not exist at path: $videoPath");
       throw Exception("Video file not found");
     }
-
-    // Create a reference to the location you want to upload to in Firebase Storage
+    
     final Reference ref = _storage.ref().child('memories/$videoTitle.mp4');
 
     try {
-      // Upload the file
-      clearPrint("Starting upload for file: $videoPath");
       final UploadTask uploadTask = ref.putFile(file);
 
-      // Wait until the file is uploaded
       await uploadTask;
 
       // Get the download URL
       final String downloadURL = await ref.getDownloadURL();
-      clearPrint("Upload successful. Download URL: $downloadURL");
+      await _saveVideoMetadata(downloadURL, username, duration);
+
     } on FirebaseException catch (e) {
       clearPrint("Firebase Exception: ${e.code} - ${e.message}");
       rethrow;
     } catch (e) {
       clearPrint("Unexpected error during upload: $e");
+      rethrow;
+    }
+  }
+
+  Future<void> _saveVideoMetadata(String videoUrl, String username, int duration) async {
+    try{
+      clearPrint("Saving video metadata");
+      await FirebaseFirestore.instance.collection('videos').add({
+        'url': videoUrl,
+        'username': username,
+        'duration': duration, // in seconds
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    }catch(exception){
+      clearPrint("Error saving video metadata: $exception");
       rethrow;
     }
   }
