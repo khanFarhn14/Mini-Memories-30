9c0b656416f6d71f3f22540c702d3d91060b491d
khanFarhn14 <thecubeshade21@gmail.com>
2024-08-01 11:22:50 +0530

    feat(handle exception): added more exception to handle empty docs as well as socket exception

--- lib/backend/fetch_file.dart
+++ lib/backend/fetch_file.dart
@@ -1,15 +1,22 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mini_memories_30/user_interface/pages/page_upload_video.dart';
 
 class FetchFile{
   final database = FirebaseFirestore.instance;
 
   Future<QuerySnapshot<Map<String, dynamic>>> getAllContent() async{
     try{
-      final content = await database.collection('videos').get();
-      clearPrint("Fetch all content: ${content.docs[0]['username']}");
-      clearPrint("Length of the fetched content: ${content.docs.length}");
-      return content;
+      final collection = await database.collection('videos').get();
+      
+      // if the collection is empty then throw exception
+      if(collection.docs.isEmpty){
+        throw Exception('No feed to display');
+      }
+
+      return collection;
+    }on SocketException{
+      throw const SocketException('An error occurred while fetching');
     }catch(exception){
       rethrow;
     }
